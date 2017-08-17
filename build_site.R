2b530f439a6376e47270671726c6dd137ac1d441
ThembaM <tmutemaringa@yahoo.com>
2017-08-17 11:40:40 +0200

    Add files via upload

--- build_site.R
+++ build_site.R
@@ -1,5 +1,8 @@
-#Set our working directory.
-setwd("C:/Users/19800701/personal_site")
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/19800701/Documents/RMarkdown/personal_site")
 
 #render your sweet site. 
 rmarkdown::render_site()
